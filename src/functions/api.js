361c78ad7fbe0c4eac98b19725551dfc7062ab72
Rangka Empat Studio <rangkaempatstudio@gmail.com>
2025-05-31 15:37:53 +0800

    Adjust backend base URL

--- src/functions/api.js
+++ src/functions/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5001/api", // Adjust to your backend base URL
+  baseURL: "http://localhost:10000/api", // Adjust to your backend base URL
 });
 
 export default api;
